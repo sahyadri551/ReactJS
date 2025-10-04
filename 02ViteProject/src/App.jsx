43ab25b852de66991a628cf4ba1948c5d5769348
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-10-04 15:23:50 +0530

    update App component to include variable in fragment and add MyApp component in main

--- 02ViteProject/src/App.jsx
+++ 02ViteProject/src/App.jsx
@@ -1,12 +1,11 @@
 import Smt from "./001newCompo"
 function App() {
-  // const [count, setCount] = useState(1)
-
+ const var1 = "Variable";
   return (
     <> 
       {/* returning fragment because i want to return multiple elements but i can only return one element. */}
       <Smt />
-      <h2>Fragment Returned</h2>
+      <h2>Fragment Returned{var1}</h2>
     </>
   )
 }
--- 02ViteProject/src/main.jsx
+++ 02ViteProject/src/main.jsx
@@ -1,7 +1,33 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <>
+        <h1>Custom App</h1>
+        </>
+    )
+    
+}
+
+const anathorElem = (
+    <div>
+        <h1>Custom App</h1>
+    </div>
+)
+
+const elem = React.createElement(
+    'h1',
+    {id:'title'},
+    'Hello World'
+)
+
 createRoot(document.getElementById('root')).render(
+    <>
     <App />
+    <MyApp />
+    {elem}
+    {anathorElem}
+    </>
 )
