882055fd614215f67f34e3d56418961d0c7c3e32
sahyadri551 <sahyadri2413082@akgec.ac.in>
2025-10-19 08:27:09 +0530

    feat: Initialize Currency Converter project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and Vite.
    - Created main application structure including App component, InputBox component, and custom hook for fetching currency data.
    - Implemented basic styling with Tailwind CSS and custom CSS.
    - Set up Vite configuration for React and Tailwind CSS integration.
    - Added SVG assets for branding.

---
+++ 07CurrencyConvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+    const [currencyInfo, setCurrencyInfo] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+            .then(res => res.json())
+            .then(data => setCurrencyInfo(data[currency]))
+    }, [currency])
+    return currencyInfo;
+}
+
+export default useCurrencyInfo;
